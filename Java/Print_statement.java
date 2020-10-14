df3b9bfb6d5089dfd4def187103ceff56ff58e2f
Gloria Thomas <50512131+gloria2000@users.noreply.github.com>
2020-10-14 21:01:39 +0530

    Create Print_statement.java

---
+++ Java/Print_statement.java
@@ -0,0 +1,11 @@
+class Print{
+  public static void main( String args[]){
+      System.out.println("The following is the code required to print "Hello" in java:\n
+      class Print{\n
+        public static void main( String args[]){\n
+            System.out.println("Hello human!");\n
+            }\n
+        }\n
+   }
+}
+      
