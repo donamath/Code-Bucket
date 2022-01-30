6016b014828cd070e3df57fadf7163c5fbc56005
Sid <siddiqkaithodu007@gmail.com>
2022-01-30 08:08:02 +0530

    Performance issues
    Use in instead of using multiple ==

--- Python/diceGame.py
+++ Python/diceGame.py
@@ -2,7 +2,7 @@ import random                                                 #Python library th
 
 choice=input("Shake Die?(Y/N)")                               #Asking user whether he wants to shake the die or not
 
-while(choice=='y' or choice=='Y'):                            #If he chooses to shake the die the following 3 statements are executed
+while choice in ('y','Y'):                            #If he chooses to shake the die the following 3 statements are executed
     number=random.randint(1,6)
     print("The die landed on "+str(number)+"!!!")             #Printing the number on which the die landed on
     choice=input("Shake Die?(Y/N)")                           #Asking user if he wants to shake the die one more time?
